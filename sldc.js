4291e4b2c54770993050a008929bb76f38e01962
Colby Workman <cgw2319@gmail.com>
2021-10-15 09:46:03 -0600

    sldc.js complete

--- sldc.js
+++ sldc.js
@@ -13,7 +13,26 @@
 // Testing & Integration
 
 
+
+//Planning
+//Analysis of Requirements
+//Design
+//Implementation
+//Testing and Integration
+//Maintain
+
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let planning = "Devs plan for the upcoming project.";
+let analysisOfRequirements = "Gather details about specs for software, hardware, network requirements and the needs of end-users.";
+let design = "Devs outline details for overall application and user/system interfaces, network, and databases.";
+let implementation = "Devs implement their plans and designs and build the software.";
+let testingAndIntegration = "The software is looked over and tested for bugs/defects and the user experience is tested. After testing, it is integrated into its environment and eventually installed.";
+let maintain = "Developers do maintenance on the software, handle reported issues, and update it.";
+
+sldc = [planning, analysisOfRequirements, design, implementation, testingAndIntegration, maintain];
+console.log(sldc);
